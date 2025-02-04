ab0c7d846ce9e19c912685b93d21b42e537613ae
LaraSamara <larasamara2002@gmail.com>
2025-02-04 15:01:42 +0200

    CA-7: Refactor Code

--- src/hooks/useGrid.tsx
+++ src/hooks/useGrid.tsx
@@ -4,7 +4,7 @@ import { GridContext } from "@clinic/context/GridContext";
 const useGrid = () => {
     const context = useContext(GridContext);
     if(!context){
-        throw new Error("GridContext should be wrapped inside the snackbar provider");
+        throw new Error("GridContext should be wrapped inside the Grid provider");
     }
     return context;
 };
