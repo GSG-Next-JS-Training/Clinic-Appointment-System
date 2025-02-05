fd850b0b00a174c9375fc8e6fdcba30691e14c3e
Eng-MaysQasem <mbqasem7@gmail.com>
2025-02-05 19:28:54 +0200

    CA-15:Add Loader Page After Refactor

--- src/component/Loader/Loader.tsx
+++ src/component/Loader/Loader.tsx
@@ -1,23 +1,28 @@
 "use client";
 import React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 import classes from "./style.module.css";
+
 interface LoaderProps {
   size?: number;
   color?: "primary" | "secondary" | "inherit";
   loading?: boolean;
 }
 
-const Loader: React.FC<LoaderProps> = ({
+const Loader: React.FC<Partial<LoaderProps>> = ({
   size = 60,
   color = "primary",
   loading = true,
 }) => {
+
   if (!loading) return null;
+  
+
   return (
-    <div className={classes.loaderContainer}>
+    <Box className={classes.loaderContainer}>
       <CircularProgress size={size} color={color} />
-    </div>
+    </Box>
   );
 };
 
