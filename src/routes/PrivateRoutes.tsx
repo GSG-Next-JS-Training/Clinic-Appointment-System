33626661c4c8d6200fc2a638f9055a81491ab968
Eng-MaysQasem <mbqasem7@gmail.com>
2025-02-11 00:01:18 +0200

    CA-25:Add Navbar Component After Refactor With Private Route Layout

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -4,21 +4,18 @@ import Appointments from "@clinic/pages/appointments/appointments";
 
 const privateRoutes: RouteObject = {
   path: "/clinic",
+  element: <GardedRoute />,
   children: [
     {
-      element: <GardedRoute />,
-      children: [
-        {
-          index: true,
-          element: <h1>Landing Page</h1>,
-        },
-        {
-          path: "appointments-dashboard",
-          element: <Appointments />,
-        },
-      ],
+      index: true,
+      element: <h1>Landing Page</h1>,
+    },
+    {
+      path: "appointments-dashboard",
+      element: <Appointments />,
     },
   ],
 };
 
 export default privateRoutes;
+
