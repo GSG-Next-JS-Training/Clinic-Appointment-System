512c32ae807b075de3401a9ddc7a19d935ed7f65
Hajar Alhajeh <hajar.ihab@gmail.com>
2025-02-14 17:01:28 +0200

    improvements: change the color

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -11,15 +11,14 @@ import { Tooltip } from "@mui/material";
 export default function Navbar() {
   return (
       <Box sx={{ flexGrow: 1}}>
-        <AppBar position="static" sx={{ backgroundColor: "#4fc3f7" }}>
+        <AppBar position="static" sx={{ backgroundColor:  "#87BFF7"  }}>
           <Toolbar>
             <Avatar
               alt="Clinic Logo"
               src="/clinic-appointment-system2-high-resolution-logo.png"
-              sx={{ height: "50px", width: "50px", mr: 1 }}
+              sx={{ height: "50px", width: "50px", mr: 1}}
               variant="circular"
             />
-
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Clinic Appointment System
             </Typography>
